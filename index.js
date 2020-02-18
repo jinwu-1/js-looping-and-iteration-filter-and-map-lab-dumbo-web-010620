6413026747e5dae820e8833d4a58fbf780d932ee
Jin Wu <jinn.wuu@gmail.com>
2020-02-18 11:54:17 -0500

    Done.

--- index.js
+++ index.js
@@ -1,33 +1,29 @@
 // Code your solution here:
 
 function driversWithRevenueOver(drivers, revenue) {
-    return drivers.filter(function (driver){
+    return drivers.filter(function (driver) {
         return driver.revenue > revenue
     })
 }
 
-function driverNamesWithRevenueOver (drivers, revenue) {
-    return driversWithRevenueOver(drivers, revenue)
-        .map(function (driver) {
-            return driver.name;
-    });
+function driverNamesWithRevenueOver(drivers, revenue) {
+    return driversWithRevenueOver(drivers, revenue).map(function (driver){
+        return driver.name
+    })
 }
 
-function exactMatch (drivers, matcher) {
+function exactMatch(drivers, matcher) {
     return drivers.filter(function (driver) {
-        let matches = false;
-  
+        let match = false
         for (const key in matcher) {
-            matches = driver[key] === matcher[key];
+            match = driver[key] === matcher[key]
         }
-  
-        return matches;
-    });
+        return match
+    })
 }
 
-function exactMatchToList (drivers, matcher) {
-    return exactMatch(drivers, matcher)
-        .map(function (driver) {
-            return driver.name;
-    });
+function exactMatchToList(drivers, matcher) {
+    return exactMatch(drivers, matcher).map(function (driver){
+        return driver.name
+    })
 }
